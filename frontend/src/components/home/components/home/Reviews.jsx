958211c7523cc826ed43fbc02ea37fa0e1c79fca
Eternatus <anikroy173@gmail.com>
2023-12-02 22:51:18 +0530

    updates

--- frontend/src/components/home/components/home/Reviews.jsx
+++ frontend/src/components/home/components/home/Reviews.jsx
@@ -89,12 +89,7 @@ export default function Reviews() {
               className="text-xl !font-sans text-gray-600"
               data-aos="zoom-y-out"
             >
-              With over 20,000 satisfied clients all over Egypt, E-Bank is the
-              ultimate banking solution for a fast, secure, and convenient
-              banking experience. Say goodbye to long queues, paperwork, and
-              outdated systems. Our innovative technology and personalized
-              service make banking easier and more enjoyable than ever. Join the
-              E-Bank revolution today and take control of your finances.{" "}
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum eum modi nam omnis rem totam velit. Architecto asperiores atque, dignissimos dolores est et id itaque mollitia non, officia optio quia quibusdam, quisquam rem saepe similique suscipit ut veniam? A accusantium amet architecto, at consequuntur cum dicta dignissimos dolorum eius esse in ipsum labore magnam modi nam officiis provident quam quasi quidem repudiandae sapiente sit sunt tempora temporibus totam ullam vel veritatis? Dicta eum expedita magni mollitia numquam officiis recusandae sunt?{" "}
             </p>
           </header>
 
@@ -127,12 +122,7 @@ export default function Reviews() {
                     {" "}
                     <FaQuoteLeft size={30} className="mr-2" />{" "}
                   </span>{" "}
-                  E-Bank has made banking so easy for me! The multiple account
-                  feature is wonderful, and I can transfer, withdraw, and
-                  deposit money anywhere and anytime. The support service is
-                  also top-notch, always ready to assist with any questions or
-                  concerns. E-Bank has truly revolutionized the way I manage my
-                  finances.{" "}
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolore facere id minima porro. Aliquam aliquid corporis, cupiditate debitis dolorum enim magnam quam rem ullam.{" "}
                   <span className="text-orange-500">
                     {" "}
                     <FaQuoteRight size={30} className="ml-2" />{" "}
--- frontend/src/components/shared/Logo.jsx
+++ frontend/src/components/shared/Logo.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import { FcLightAtTheEndOfTunnel } from "react-icons/fc";
+import bank from "../../assets/icons/E-BANK-Logo.png";
 
 export const Logo = ({ textSize = null, bg = true }) => {
   return (
     <div
       className={`${
-        bg && "bg-blue-200 border-x-4 border-blue-800  shadow rounded"
+        bg && "shadow rounded"
       } w-full flex justify-center items-center p-2  select-none`}
     >
-      <FcLightAtTheEndOfTunnel className="-mr-2" size={45} />
-      <span
-        className={`${
-          textSize ? textSize : "text-3xl"
-        } p-1  text-blue-800 font-bold `}
-      >
-        E-BANK
-      </span>
+      <img src={bank} alt={"text"} height={120} width={120}/>
     </div>
   );
 };
